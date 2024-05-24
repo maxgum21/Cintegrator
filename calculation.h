7909b4ded43e7a2caae50db79c35265031787397
maxgum21 <maxgumyasko@gmail.com>
2024-05-24 18:10:00 +0300

    Slight structure changes

--- calculation.h
+++
@@ -1,7 +0,0 @@
-extern unsigned n;          // n - iteration counter
-
-double integral(double (*f)(double), double a, double b, double eps2);
-
-double root_tangent(double (*f)(double), double (*df)(double), double (*g)(double), double (*dg)(double), double a, double b, double eps1);
-
-double root_chord(double (*f)(double), double (*g)(double), double a, double b, double eps1);
--- functions.h
+++
@@ -1,5 +0,0 @@
-extern double (*func_arr[])(double);
-
-extern int num;
-
-extern double a, b;
